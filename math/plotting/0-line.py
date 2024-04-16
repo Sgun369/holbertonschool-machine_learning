99d5fadd967c4c0e1ec96dc7b3876f4309d1cfb3
Sgun369 <souhaiebjebali@gmail.com>
2024-04-16 06:05:33 -0400

    0-line

--- math/plotting/0-line.py
+++ math/plotting/0-line.py
@@ -6,6 +6,7 @@ import matplotlib.pyplot as plt
 def line():
 
     y = np.arange(0, 11) ** 3
-    x = np.arange(0, 11)
-    plt.plot(x, y, color='red')
+    plt.figure(figsize=(6.4, 4.8))
+    plt.plot(y, color='red')
+    plt.xlim = 10
     plt.show()
