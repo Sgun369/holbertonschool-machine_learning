ae22e455a301574b65ebf7fcbff55226d40842c8
Sgun369 <souhaiebjebali@gmail.com>
2024-04-16 06:09:48 -0400

    0-line

--- math/plotting/0-line.py
+++ math/plotting/0-line.py
@@ -1,10 +1,11 @@
 #!/usr/bin/env python3
+"""Line Graph"""
 import numpy as np
 import matplotlib.pyplot as plt
 
 
 def line():
-
+    """Line plot"""
     y = np.arange(0, 11) ** 3
     plt.figure(figsize=(6.4, 4.8))
     plt.plot(y, color='red')
