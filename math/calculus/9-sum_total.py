b9a9234c03f380ec87b74593b11ca0aef817727b
Sgun369 <souhaiebjebali@gmail.com>
2024-04-21 23:37:50 +0100

    9-sum_total.py

---
+++ math/calculus/9-sum_total.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+""" Our life is the sum total of all the decisions we make every day,
+and those decisions are
+determined by our priorities"""
+
+
+def summation_i_squared(n):
+    if not isinstance(n, (int, float)) or n < 1:
+        return None
+
+    if not isinstance(n, int):
+        return None
+
+    return n * (n + 1) * (2 * n + 1) // 6
