7a976336e084ebf0620ef39e9f3919cec4b33a84
Sgun369 <souhaiebjebali@gmail.com>
2024-05-01 05:13:03 -0400

    neuron

---
+++ supervised_learning/classification/0-neuron.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+"""Neuron """
+import numpy as np
+
+
+class Neuron:
+    """Represent a single neuron for binary classification"""
+
+    def __init__(self, nx):
+        """initialize the neuron"""
+        if not isinstance(nx, int):
+            raise TypeError("nx mus be an integer")
+        if nx < 1:
+            raise ValueError("nx must be a positive integer")
+
+        self.W = np.random.randn(nx).reshape((1, nx))
+        self.b = 0
+        self.A = 0
