898e9c9f6ed74625bf651d839bd9402fe64cf9c1
Sgun369 <souhaiebjebali@gmail.com>
2024-05-04 10:19:51 +0100

    6-neuron.py

--- supervised_learning/classification/6-neuron.py
+++ supervised_learning/classification/6-neuron.py
@@ -67,15 +67,15 @@ class Neuron:
     def train(self, X, Y, iterations=5000, alpha=0.05):
         """Trains the neuron"""
         if not isinstance(iterations, int):
-            raise TypeError("iterations mustt be an integer")
+            raise TypeError("iterations must be an integer")
         if iterations < 0:
-            raise ValueError("iterations mustt be a positive integer")
+            raise ValueError("iterations must be a positive integer")
         if not isinstance(alpha, float):
             raise TypeError("alpha must be a float")
         if alpha < 0:
             raise ValueError("alpha must be positive")
-
         for _ in range(iterations):
-            self.__A = self.forward_prop(X)
+            self.forward_prop(X)
             self.gradient_descent(X, Y, self.__A, alpha)
+
         return self.evaluate(X, Y)
