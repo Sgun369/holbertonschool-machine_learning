60801c67166c1b0cf0866abb42cf982caf4ffbd1
Sgun369 <souhaiebjebali@gmail.com>
2024-05-07 17:35:47 +0100

    16-deep_neural_network.py

--- supervised_learning/classification/16-deep_neural_network.py
+++ supervised_learning/classification/16-deep_neural_network.py
@@ -1,27 +1,22 @@
 #!/usr/bin/env python3
-"""DeepNeuralNetwork"""
+""" Creating the deep neural network """
 import numpy as np
 
 
 class DeepNeuralNetwork:
-    """class DeepNeuralNetwork"""
-
+    """ deep neural network performing binary classification """
     def __init__(self, nx, layers):
-        """constructor to initialize
-        the deep neural network"""
+        """ Class constructor """
         if not isinstance(nx, int):
             raise TypeError("nx must be an integer")
         if nx < 1:
             raise ValueError("nx must be a positive integer")
-        if not isinstance(layers, list) or not layers:
-            raise TypeError("layers must be a list of positive integers")
-        if any(not isinstance(layer, int) or layer < 1 for layer in layers):
+        if not isinstance(layers, list) or layers == []:
             raise TypeError("layers must be a list of positive integers")
 
-        self.L = len(layers)  # number of layers
-        self.cache = {}  # intermediary values of the network
-        self.weights = {}  # weights and biases of the network
-        # loop through each layer to initialize weights and biases
+        self.L = len(layers)
+        self.cache = dict()
+        self.weights = dict()
         for i in range(len(layers)):
             if (not isinstance(layers[i], int)) or layers[i] <= 0:
                 raise TypeError("layers must be a list of positive integers")
@@ -33,3 +28,4 @@ class DeepNeuralNetwork:
                     layers[i], layers[i - 1]) * np.sqrt(2 / layers[i - 1])
                 self.weights['W' + str(i + 1)] = He
             self.weights['b' + str(i + 1)] = np.zeros((layers[i], 1))
+         
