6e4b4cd887a73902d5fa6aa235a9ae13718622a5
Sgun369 <souhaiebjebali@gmail.com>
2024-05-07 13:19:00 +0100

    14-neural_network.py

--- supervised_learning/classification/14-neural_network.py
+++ supervised_learning/classification/14-neural_network.py
@@ -101,7 +101,7 @@ class NeuralNetwork:
         """Trains the neural network"""
         if not isinstance(iterations, int):
             raise TypeError("iterations must be an integer")
-        if iterations <= 0:
+        if iterations < 0:
             raise ValueError("iterations must be a poositive integer")
         if not isinstance(alpha, float):
             raise TypeError("alpha must be a float")
