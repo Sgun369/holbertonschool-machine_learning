2ae0dc701bf14674c5ea8c2b24fe6583efbe72ad
Sgun369 <souhaiebjebali@gmail.com>
2024-05-26 13:06:11 +0100

    1-l2_reg_gradient_descent.pyl

--- supervised_learning/regularization/1-l2_reg_gradient_descent.py
+++ supervised_learning/regularization/1-l2_reg_gradient_descent.py
@@ -7,20 +7,33 @@ def l2_reg_gradient_descent(Y, weights, cache, alpha, lambtha, L):
     """updates the weights and biases
     of a neural network using gradient descent
     with L2 regularization"""
-    m = Y.shape[1]
+    num_examples = Y.shape[1]
 
-    dZ = cache[f'A{L}'] - Y
+    # Compute delta for the output layer
+    delta_output_layer = cache[f'A{L}'] - Y
 
     for layer in range(L, 0, -1):
-        A_prev = cache[f'A{layer-1}'] if layer > 1 else cache['A0']
+        current_weight_key = f'W{layer}'
+        current_bias_key = f'b{layer}'
+        current_activation_key = f'A{layer}'
+        prev_activation_key = f'A{layer-1}' if layer > 1 else 'A0'
 
-    dW = (1 / m) * np.dot(dZ, A_prev.T) + (lambtha / m) * weights[f'W{layer}']
-    db = (1 / m) * np.sum(dZ, axis=1, keepdims=True)
+        current_activation = cache[current_activation_key]
+        prev_activation = cache[prev_activation_key]
 
-    if layer > 1:
-        W = weights[f'w{i}']
-        dA_prev = np.dot(weights[f'W{layer}'].T, dZ)
-        dZ = dA_prev * (1 - A_prev ** 2)
+        # Calculate gradients for the weights and biases
+        weight_gradient = (1 / num_examples) * np.dot(delta_output_layer,
+                                                      prev_activation.T) + (lambtha / num_examples) * weights[current_weight_key]
+        bias_gradient = (1 / num_examples) * \
+            np.sum(delta_output_layer, axis=1, keepdims=True)
 
-    weights[f'W{layer}'] -= alpha * dW
-    weights[f'b{layer}'] -= alpha * db
+        if layer > 1:
+            # Compute delta for the previous layer
+            weight_matrix = weights[current_weight_key]
+            delta_prev_layer = np.dot(weight_matrix.T, delta_output_layer)
+            delta_output_layer = delta_prev_layer * \
+                (1 - prev_activation ** 2)  # Derivative of tanh
+
+        # Update weights and biases
+        weights[current_weight_key] -= alpha * weight_gradient
+        weights[current_bias_key] -= alpha * bias_gradient
