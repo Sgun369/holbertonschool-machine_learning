73c0525dcdd176a426b6c18adf4a231c37aec20c
Sgun369 <souhaiebjebali@gmail.com>
2024-05-30 10:33:05 +0100

    3-one_hot.py

---
+++ supervised_learning/keras/3-one_hot.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"""One Hot"""
+import tensorflow.keras as K
+import numpy as np
+
+
+def one_hot(labels, classes=None):
+    """converts a label vector into a one-hot matrix"""
+    one_hot_matrix = K.utils.to_categorical(labels, num_classes=classes)
+    return one_hot_matrix
