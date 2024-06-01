e1843ba7b4110f02ee516a93aa7144b4eca4e7a4
Sgun369 <souhaiebjebali@gmail.com>
2024-06-01 11:36:21 +0100

    4-convolve_channels.py

--- math/convolutions_and_pooling/4-convolve_channels.py
+++ math/convolutions_and_pooling/4-convolve_channels.py
@@ -8,31 +8,23 @@ def convolve_channels(images, kernel, padding='same', stride=(1, 1)):
     m, h, w, c = images.shape
     kh, kw, kc = kernel.shape
     sh, sw = stride
-
-    if padding == 'same':
-        ph = ((h - 1) * sh + kh - h) // 2
-        pw = ((w - 1) * sw + kw - w) // 2
-    elif padding == 'valid':
+    if padding == 'valid':
         ph, pw = 0, 0
-    else:
+    elif padding == 'same':
+        ph = (((h - 1) * sh + kh - h) // 2) + 1
+        pw = (((w - 1) * sw + kw - w) // 2) + 1
+    elif isinstance(padding, tuple):
         ph, pw = padding
 
-    padded_images = np.pad(
-        images, ((0, 0), (ph, ph), (pw, pw), (0, 0)), mode='constant')
+    pad_img = np.pad(images, ((0, 0), (ph, ph), (pw, pw), (0, 0)))
 
-    output_h = (h + 2 * ph - kh) // sh + 1
-    output_w = (w + 2 * pw - kw) // sw + 1
+    out_h = ((h + 2 * ph - kh) // sh) + 1
+    out_w = ((w + 2 * pw - kw) // sw) + 1
 
-    output = np.zeros((m, output_h, output_w))
-
-    for i in range(output_h):
-        for j in range(output_w):
-            start_i = i * sh
-            start_j = j * sw
-            image_slice = padded_images[:,
-                                        start_i:start_i + kh,
-                                        start_j:start_j + kw,
-                                        :]
-            output[:, i, j] = np.sum(image_slice * kernel, axis=(1, 2, 3))
-
+    output = np.zeros((m, out_h, out_w))
+    for i in range(out_h):
+        for j in range(out_w):
+            output[:, i, j] = np.sum(
+                pad_img[:, i * sh:i * sh + kh, j * sw:j * sw + kw, :]
+                * kernel, axis=(1, 2, 3))
     return output
