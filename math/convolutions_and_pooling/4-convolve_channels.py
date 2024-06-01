0c16fbec883cd82edb5013d4553cca9c2d619451
Sgun369 <souhaiebjebali@gmail.com>
2024-06-01 11:33:50 +0100

    4-convolve_channels.py

---
+++ math/convolutions_and_pooling/4-convolve_channels.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python3
+"""Convolution with Channels"""
+import numpy as np
+
+
+def convolve_channels(images, kernel, padding='same', stride=(1, 1)):
+    """performs a convolution on images with channels"""
+    m, h, w, c = images.shape
+    kh, kw, kc = kernel.shape
+    sh, sw = stride
+
+    if padding == 'same':
+        ph = ((h - 1) * sh + kh - h) // 2
+        pw = ((w - 1) * sw + kw - w) // 2
+    elif padding == 'valid':
+        ph, pw = 0, 0
+    else:
+        ph, pw = padding
+
+    padded_images = np.pad(
+        images, ((0, 0), (ph, ph), (pw, pw), (0, 0)), mode='constant')
+
+    output_h = (h + 2 * ph - kh) // sh + 1
+    output_w = (w + 2 * pw - kw) // sw + 1
+
+    output = np.zeros((m, output_h, output_w))
+
+    for i in range(output_h):
+        for j in range(output_w):
+            start_i = i * sh
+            start_j = j * sw
+            image_slice = padded_images[:,
+                                        start_i:start_i + kh,
+                                        start_j:start_j + kw,
+                                        :]
+            output[:, i, j] = np.sum(image_slice * kernel, axis=(1, 2, 3))
+
+    return output
