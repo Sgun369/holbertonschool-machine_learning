ef78b24ae4c6a530d073ad290036f979741eabd4
Sgun369 <souhaiebjebali@gmail.com>
2024-06-12 08:06:13 +0100

    0-inception_block.py

--- supervised_learning/deep_cnns/0-inception_block.py
+++ supervised_learning/deep_cnns/0-inception_block.py
@@ -5,7 +5,8 @@ from tensorflow import keras as K
 
 
 def inception_block(A_prev, filters):
-
+    """ builds an inception block as described in
+    Going Deeper with Convolutions (2014)"""
     F1, F3R, F3, F5R, F5, FPP = filters
     conv1x1 = K.layers.Conv2D(
         F1, (1, 1), padding='same', activation='relu')(A_prev)
