c479398e1f9f0bf69f42950e215de215198af759
Sgun369 <souhaiebjebali@gmail.com>
2024-12-03 14:59:55 +0100

    2-cofactor.py

---
+++ math/advanced_linear_algebra/2-cofactor.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python3
+"""module to calculate the cofactor of a matrix"""
+
+determinant = __import__('0-determinant').determinant
+
+
+def cofactor(matrix):
+    """
+    Calculates the cofacttor matrix of  a given square matrix
+    """
+    if not isinstance(
+        matrix,
+        list) or not all(
+        isinstance(
+            row,
+            list) for row in matrix):
+        raise TypeError("matrix must be a list of lists")
+
+    rows = len(matrix)
+    if rows == 0 or any(len(row) != rows for row in matrix):
+        raise ValueError("matrix must be a non-empty square matrix")
+
+    cofactor_matrix = []
+    for i in range(rows):
+        cofactor_row = []
+        for j in range(rows):
+            sub_matrix = [row[:j] + row[j + 1:]
+                          for idx, row in enumerate(matrix) if idx != i]
+            cofactor_value = ((-1) ** (i + j)) * determinant(sub_matrix)
+            cofactor_row.append(cofactor_value)
+            cofactor_matrix.append(cofactor_row)
+    return cofactor_matrix
