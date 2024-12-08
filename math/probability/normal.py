9f1b15538fd9c74aa1c8629e261140abedaa19d3
Sgun369 <souhaiebjebali@gmail.com>
2024-12-08 11:08:49 +0100

    normal.py

--- math/probability/normal.py
+++ math/probability/normal.py
@@ -28,3 +28,11 @@ class Normal:
     def x_value(self, z):
         """calculates the c-value of a given z-score"""
         return z * self.stddev + self.mean
+
+    def pdf(self, x):
+        """calculates the value of the PDF for a given x-value"""
+        pi = 3.1415926536
+        e = 2.7182818285
+        exponent = -((x - self.mean) ** 2) / (2 * (self.stddev ** 2))
+        coefficient = 1 / (self.stddev * (2 * pi) ** 0.5)
+        return coefficient * e ** exponent
