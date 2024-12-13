53e597116b2eb51b1062fe4aa09482758ebcd2f0
Sgun369 <souhaiebjebali@gmail.com>
2024-12-13 11:57:43 +0100

    4

--- unsupervised_learning/clustering/4-initialize.py
+++ unsupervised_learning/clustering/4-initialize.py
@@ -7,10 +7,12 @@ kmeans = __import__('1-kmeans').kmeans
 def initialize(X, k):
     """ initializes variables for a Gaussian Mixture Model"""
     try:
+        if not isinstance(k, int) or k <= 0:
+            return None, None, None
         pi = np.full((k,), 1 / k)
         m, _ = kmeans(X, k)
         d = X.shape[1]
-        S = np.array([np.eye(d) for _ in range(k)])
+        S = np.array([np.eye(d)] * k)
         return pi, m, S
     except Exception:
         return None, None, None
