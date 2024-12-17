8f0d33ea553cd1282d80a63e52a2678957ba34cc
Sgun369 <souhaiebjebali@gmail.com>
2024-12-17 14:22:30 +0100

    11

---
+++ unsupervised_learning/clustering/11-gmm.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+"""module GMM"""
+import numpy as np
+import sklearn.mixture
+
+
+def gmm(X, k):
+    """calculates a gaussian misture Model from a dataset"""
+    gmm = sklearn.mixture.GaussianMixture(n_components=k)
+    gmm.fit(X)
+
+    pi = gmm.weights_
+    m = gmm.means_
+    S = gmm.covariances_
+    clss = gmm.predict(X)
+    bic = gmm.bic(X)
+    return pi, m, S, clss, bic
