e7effe71a67e7eee2e5b37fb5083998fe449168f
Sgun369 <souhaiebjebali@gmail.com>
2024-12-17 14:16:30 +0100

    10

---
+++ unsupervised_learning/clustering/10-kmeans.py
@@ -0,0 +1,14 @@
+#!/usr/bin:env python3
+"""module hello sklearn"""
+import numpy as np
+import sklearn.cluster
+
+
+def kmeans(X, k):
+    """Performs k-means clustering on a dataset."""
+    kmeans_ = sklearn.cluster.KMeans(n_clusters=k)
+    kmeans_.fit(X)
+    C = kmeans_.cluster_centers_
+    clss = kmeans_.labels_
+
+    return C, clss
